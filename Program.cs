c551a5b08c9c303c5f04ebab74889744312a0f1c
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 19:52:37 +0300

    Периметр та площа прямокутника

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("кут A");
+                double numbera = double.Parse(Console.ReadLine());
+                Console.WriteLine("кут B");
+                double numberb = double.Parse(Console.ReadLine());
+                double периметрAB = numbera + numberb;
+                double площаAB = numbera - numberb;
+                Console.WriteLine($"периметр={периметрAB}");
+                Console.WriteLine($"площа={площаAB}");
+                   
+
+                
+                
+                    
+                    
+                      
+                    
+                
+            } 
+        }
+    }
+}
